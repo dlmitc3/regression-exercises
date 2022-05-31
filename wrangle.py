ebf4b3932da23df1b9c65b72526e181369d5726a
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-05-31 14:26:08 -0500

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -21,40 +21,18 @@ def wrangle_zillow(df):
     # show only data from the selected columns
     df1 = df[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt', 'yearbuilt', 'taxamount', 'fips']]
      
-    # Display readable summary statistics for numeric columns. Why isn't exam3 showing up?
+    # Display readable summary statistics for numeric columns.
     df1.describe().T
 
-    # Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df.
+    # Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df1.
     df1 = df1.replace(r'^\s*$', np.nan, regex=True)
 
-    # Drop all rows with any Null values, assign to df, and verify.
+    # Drop all rows with any Null values, assign to df1, and verify.
     df1 = df1.dropna()
 
-    # Change all column data tyes to int64, reassign to df, and verify.
+    # Change all column data tyes to int64, reassign to df1, and verify.
     df1 = df1.astype('int')
 
     df1.describe().T
 
-    plt.figure(figsize=(21, 2))
-
-# List of columns
-cols = ['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt','yearbuilt','taxamount','fips']
-
-for i, col in enumerate(cols):
-
-    # i starts at 0, but plot nos should start at 1
-    plot_number = i + 1 
-
-    # Create subplot.
-    plt.subplot(1,7, plot_number)
-
-    # Title with column name.
-    plt.title(col)
-
-    # Display histogram for column.
-    df1[col].hist(bins=10)
-
-    # Hide gridlines.
-    plt.grid(False)
-
-    return df
+     return df
