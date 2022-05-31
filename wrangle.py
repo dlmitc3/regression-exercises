314884a07503f70a52b4011cdfae0cf31ee267de
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-05-31 16:20:54 -0500

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -10,29 +10,30 @@ from acquire import acquire_zillow
 
 def wrangle_zillow(cached=True):
     '''
-    This function reads in iris data from Codeup database if cached == False
-    or if cached == True reads in iris df from a csv file, returns df
+    This function reads in Zillow data from Codeup database if cached == False
+    or if cached == True reads in Zillow df from a csv file, returns df
     '''
-    if cached or os.path.isfile('zillow.csv') == False:
-        df = acquire_zillow()
-    else:
-        df = pd.read_csv('zillow.csv', index_col=0)
-   
+
+     # use my aquire function to read data into a df from a csv file
+    df = acquire_zillow(cached)
+    
     # show only data from the selected columns
-        df= df[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt', 'yearbuilt', 'taxamount', 'fips']]
+    df = df[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt', 'yearbuilt', 'taxamount', 'fips']]
      
     # Display readable summary statistics for numeric columns.
-        df.describe().T
+    df.describe().T
 
     # Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df1.
-        df = df.replace(r'^\s*$', np.nan, regex=True)
+    df = df.replace(r'^\s*$', np.nan, regex=True)
 
     # Drop all rows with any Null values, assign to df1, and verify.
-        df = df.dropna()
+    df = df.dropna()
 
     # Change all column data tyes to int64, reassign to df1, and verify.
-        df = df.astype('int')
+    df = df.astype('int')
 
-        df.describe().T
+    # describe data
+    df.describe().T
 
-        return df
+    return df
+
