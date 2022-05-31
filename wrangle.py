a3b00b61d74f2a170edf78634312622ec93c7636
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-05-31 11:27:42 -0500

    acquire.py

--- wrangle.py
+++ wrangle.py
@@ -1,33 +1,18 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-import os
-from env import host, user, password
+import numpy as np
+import math
 
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
 
-
-def get_connection(db, user=user, host=host, password=password):
+def wrangle_zillow(df):
     '''
-    This function uses my info from my env file to
-    create a connection url to access the Codeup db.
-    It takes in a string name of a database as an argument.
+    This function will drop all collumns except, bedroomcnt, bathroomcnt, 
+    calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, 
+    and fips from the zillow database for all 'Single Family Residential' properties. 
     '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-
-###################### Acquire Zillow Data ######################
-
-def new_wrangle_data():
-    '''
-    This function reads the Zillow data from the Zillow.csv file..
-    '''
-    sql_query = """
-                SELECT * from properties_2017
-                join predictions_2017 using (id)
-                join properties_2016 using (id)
-                join predictions_2016 using (id)
-                """
-    
-    # Read in DataFrame from Codeup db.
-    df = pd.read_sql(sql_query, get_connection('zillow'))
-    df.to_csv('zillow_df.csv')
-    return df
-
-
+#drop duplicates columns
+    df1 = df[['bedroomcnt', 'bathroomcnt', 'calculatedfinishedsquarefeet', 'taxvaluedollarcnt', 'yearbuilt', 'taxamount', 'fips']]
+  
+    return df1
