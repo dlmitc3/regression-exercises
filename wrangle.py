4b8cef9bbf01e70dd5d3ae909293720dfeaecfcb
David Mitchell <Davidlee.Mitchell2@Gmail.com>
2022-06-02 15:42:06 -0500

    wrangle.py

--- wrangle.py
+++ wrangle.py
@@ -75,6 +75,91 @@ def clean_zillow (df):
     df[['bedroomcnt', 'yearbuilt', 'fips']] = df[['bedroomcnt', 'yearbuilt', 'fips']].astype(int)
     return df
 
+# ***************************************************************************************************
+#                                     ZILLOW DB
+# ***************************************************************************************************
+
+#acquire data for the first time
+def get_new_zillow():
+    '''
+    This function reads in the zillow data from the Codeup db
+    and returns a pandas DataFrame with columns :
+     bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips 
+    '''
+    sql_query = '''
+    SELECT parcelid, bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet,
+    taxvaluedollarcnt
+    FROM properties_2017
+    JOIN predictions_2017 as pred USING (parcelid)
+    WHERE pred.transactiondate >= '2017-05-01' AND pred.transactiondate <= '2017-08-31'
+    AND propertylandusetypeid > 259 AND propertylandusetypeid  < 266;
+    '''
+    return pd.read_sql(sql_query, get_connection('zillow'))
+
+#acquire data main function 
+def get_zillow():
+    '''
+    This function reads in telco_churn data from Codeup database, writes data to
+    a csv file if a local file does not exist, and returns a df.
+    '''
+    if os.path.isfile('zillow.csv'):
+        
+        # If csv file exists, read in data from csv file.
+        df = pd.read_csv('zillow.csv', index_col=0)
+        
+    else:
+        
+        # Read fresh data from db into a DataFrame.
+        df = get_new_zillow()
+        
+        # Write DataFrame to a csv file.
+        df.to_csv('zillow.csv')
+        
+    return df
+
+
+
+
+
+def split_data(df):
+    '''
+    take in a DataFrame and return train, validate, and test DataFrames.
+    
+    '''
+    train_validate, test = train_test_split(df, test_size=.2, random_state=123)
+    train, validate = train_test_split(train_validate, 
+                                       test_size=.3, 
+                                       random_state=123)
+    print(f'train -> {train.shape}')
+    print(f'validate -> {validate.shape}')
+    print(f'test -> {test.shape}')                                  
+    return train, validate, test
+
+def split_Xy (train, validate, test, target):
+    '''
+    This function takes in three dataframe (train, validate, test) and a target  and splits each of the 3 samples
+    into a dataframe with independent variables and a series with the dependent, or target variable.
+    The function returns 3 dataframes and 3 series:
+    X_train (df) & y_train (series), X_validate & y_validate, X_test & y_test.
+    Example:
+    X_train, y_train, X_validate, y_validate, X_test, y_test = split_Xy (train, validate, test, 'Fertility' )
+    '''
+    
+    #split train
+    X_train = train.drop(columns= [target])
+    y_train= train[target]
+    #split validate
+    X_validate = validate.drop(columns= [target])
+    y_validate= validate[target]
+    #split validate
+    X_test = test.drop(columns= [target])
+    y_test= test[target]
+
+    print(f'X_train -> {X_train.shape}               y_train->{y_train.shape}')
+    print(f'X_validate -> {X_validate.shape}         y_validate->{y_validate.shape} ')        
+    print(f'X_test -> {X_test.shape}                  y_test>{y_test.shape}') 
+    return  X_train, y_train, X_validate, y_validate, X_test, y_test
+
 
 
 def wrangle_zillow():
@@ -92,66 +177,33 @@ def wrangle_zillow():
 
 
 
-# Function for acquiring and prepping my student_grades df.
-
-def wrangle_grades():
+def miss_dup_values(df):
     '''
-    Read student_grades csv file into a pandas DataFrame,
-    drop student_id column, replace whitespaces with NaN values,
-    drop any rows with Null values, convert all columns to int64,
-    return cleaned student grades DataFrame.
+    this function takes a dataframe as input and will output metrics for missing values and duplicated rows, 
+    and the percent of that column that has missing values and duplicated rows
     '''
-    # Acquire data from csv file.
-    grades = pd.read_csv('student_grades.csv')
-    
-    # Replace white space values with NaN values.
-    grades = grades.replace(r'^\s*$', np.nan, regex=True)
-    
-    # Drop all rows with NaN values.
-    df = grades.dropna()
-    
-    # Convert all columns to int64 data types.
-    df = df.astype('int')
-    
-    return df
+        # Total missing values
+    mis_val = df.isnull().sum()
+        # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+        #total of duplicated
+    dup = df.duplicated().sum()  
+        # Percentage of missing values
+    dup_percent = 100 * dup / len(df)
+        # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+        # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(columns = {0 : 'Missing Values', 1 : '% of Total Values'})
+        # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+    mis_val_table_ren_columns.iloc[:,1] != 0].sort_values('% of Total Values', ascending=False).round(1)
+        # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+    print( "  ")
+    print (f"** There are {dup} duplicate rows that represents {round(dup_percent, 2)}% of total Values**")
+        # Return the dataframe with missing information
+    return mis_val_table_ren_columns
 
 
-
-
-
-
-def wrangle_student_math(path):
-    df = pd.read_csv(path, sep=";")
-
-    # drop any nulls
-    df = df[~df.isnull()]
-
-    # get object column names
-    object_cols = get_object_cols(df)
-
-    # create dummy vars
-    df = create_dummies(df, object_cols)
-
-    # split data
-    X_train, y_train, X_validate, y_validate, X_test, y_test = train_validate_test(
-        df, "G3"
-    )
-
-    # get numeric column names
-    numeric_cols = get_numeric_X_cols(X_train, object_cols)
-
-    # scale data
-    X_train_scaled, X_validate_scaled, X_test_scaled = min_max_scale(
-        X_train, X_validate, X_test, numeric_cols
-    )
-
-    return (
-        df,
-        X_train,
-        X_train_scaled,
-        y_train,
-        X_validate_scaled,
-        y_validate,
-        X_test_scaled,
-        y_test,
-    )
